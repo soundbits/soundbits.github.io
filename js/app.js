8dddb78c52444146c9f10a390bae75acddc49231
nicolezhu <nzhu94@gmail.com>
2015-09-19 17:02:47 -0400

    add basic user flow

--- js/app.js
+++ js/app.js
@@ -8,14 +8,60 @@ var ractive = new Ractive({
 
   // Here, we're passing in some initial data
   data: {
+  	showList: false,
+  	currentSuggestion: undefined,
   	user: {
-  		id: undefined
+  		id: undefined,
+  		episodes: [],
+  		suggestions: [
+	  		{
+	  			episodeName: 'Reply All Exploder',
+	  			show: 'Reply All'
+	  		},
+	  		{
+	  			episodeName: 'Undo Undo Undo',
+	  			show: 'Reply All'
+	  		}
+  		],
+  		rejections: []
   	},
   	shows: ['This American Life', 'Reply All', '99% Invisible', 'Radiolab', 'Morning Edition', 'Fresh Air', 'Star Talk', 'On the Media'],
   	selectedShows: []
   },
-
-  selectShow: function() {
-  	
+  getSuggestion: function() {
+  	var shows = this.get('user.suggestions');
+  	var suggestion = shows.shift();
+  	console.log(suggestion);
+  	this.set('user.suggestions', shows);
+  	return suggestion;
   }
+});
+
+ractive.on('selectShow', function(event, showName) {
+	this.push('selectedShows', showName);
+});
+
+ractive.on('showOptions', function(event) {
+	this.set('user.id', 369);
+	this.set('currentSuggestion', this.getSuggestion());
+});
+
+ractive.on('processSuggestion', function(event) {
+	console.log(event);
+	if (event.original.direction == 4) {
+		// swipe right
+		this.push('user.episodes', this.get('currentSuggestion'));
+	} else if (event.original.direction == 2) {
+		// swipe left
+		this.push('user.rejections', this.get('currentSuggestion'));
+	}
+	var nextSuggestion = this.getSuggestion();
+	if (nextSuggestion) {
+		this.set('currentSuggestion', nextSuggestion);
+	} else {
+		this.set({
+			currentSuggestion: undefined,
+			showList: true
+		});
+	}
 });
