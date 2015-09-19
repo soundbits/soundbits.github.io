ac866cd2d468df64ce0b13be60b997aed8bc32c8
nicolezhu <nzhu94@gmail.com>
2015-09-19 17:22:44 -0400

    remove some unnecessary comments

--- js/app.js
+++ js/app.js
@@ -12,14 +12,8 @@ var skipLandingPage = function () {
 	landingPage.hide();
 }
 var ractive = new Ractive({
-  // The `el` option can be a node, an ID, or a CSS selector.
   el: '#container',
-
-  // We could pass in a string, but for the sake of convenience
-  // we're passing the ID of the <script> tag above.
   template: '#template',
-
-  // Here, we're passing in some initial data
   data: {
   	showList: false,
   	currentSuggestion: undefined,
