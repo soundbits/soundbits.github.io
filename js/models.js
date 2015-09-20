6b982ff6b6505cacd157aa6d44f02c4f62caf7bf
Quoctrung Bui <quoctrungbui@Quoctrungs-MacBook-Pro.local>
2015-09-20 14:54:10 -0400

    reset

--- js/models.js
+++ js/models.js
@@ -4,8 +4,8 @@
 
 // User and preferences model
 
-// host = "https://soundbits.herokuapp.com"
-host = "http://localhost:3000"
+host = "https://soundbits.herokuapp.com"
+// host = "http://localhost:3000"
 var User = Backbone.Model.extend({
   url: function() {
     return (this.get('id')) ? host+'/users/' + this.get('id') + '.json' :
